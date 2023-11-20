6f674399dea5b629b3920d032181f35ae551955a
rahulrofficial <rahulrofficial93@gmail.com>
2023-11-20 23:53:10 +0530

    added login,logout,register

--- Crypto/exchange/urls.py
+++ Crypto/exchange/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("view_coin", views.view_coin, name="view_coin")
 ]
